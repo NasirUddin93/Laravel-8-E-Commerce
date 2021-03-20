ad72a1791a5842e883b555b61c41dbc713c7adfc
NasirUddin93 <nasir93cse@gmail.com>
2021-03-20 19:31:26 +0600

    Shopping is done successfully

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -3,16 +3,29 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Product;
+use Cart;
 
 class DetailsComponent extends Component
 {
 	public $slug;
 	public function mount($slug){
-		$this.slug=$slug;
+		$this->slug = $slug;
 	}
+	public function store($product_id,$product_name,$product_price){
+        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        session()->flash('success_message','Item added in Cart');
+        return redirect()->route('product.cart');
+    } 
     public function render()
     {
 		$product =Product::where('slug',$this->slug)->first();
-        return view('livewire.details-component',['product'=>$product])->layout('layout.base');
+		$popular_product = Product::inRandomOrder()->limit(4)->get();
+		$related_product = Product::where('category_id',$product->category_id)->inRandomOrder()->limit(5)->get();
+        return view('livewire.details-component',['product'=>$product,'popular_product'=>$popular_product,'related_product'=>$related_product])->layout('layouts.base');
     }
 }
+
+
+
+
