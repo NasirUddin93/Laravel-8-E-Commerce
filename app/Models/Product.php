023aad6e0c5fd454f551353688c5421d67305dcd
NasirUddin93 <nasir93cse@gmail.com>
2021-03-23 07:38:39 +0600

    Admin Add New Product is done successfully git status

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,8 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Product extends Model
 {
+    
     use HasFactory;
+    //public $timestamps=false;
+
 	protected $table="products";
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class,'category_id');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditCategoryComponent;
+use App\Http\Livewire\Admin\AdminProductComponent;
+use App\Http\Livewire\Admin\AdminAddProductComponent;
 use App\Http\Livewire\User\UserDashboardComponent;
 use App\Http\Livewire\DetailsComponent;
 use App\Http\Livewire\CategoryComponent;
@@ -54,4 +56,6 @@ Route::middleware(['auth:sanctum', 'verified','authadmin'])->group(function () {
     Route::get('/admin/categories',AdminCategoryComponent::class)->name('admin.categories');
     Route::get('/admin/add/category',AdminAddCategoryComponent::class)->name('admin.addcategory');
     Route::get('/admin/edit/category/{category_slug}',AdminEditCategoryComponent::class)->name('admin.editcategory');
+    Route::get('/admin/products',AdminProductComponent::class)->name('admin.products');
+    Route::get('/admin/product/add',AdminAddProductComponent::class)->name('admin.addproduct');
 });
