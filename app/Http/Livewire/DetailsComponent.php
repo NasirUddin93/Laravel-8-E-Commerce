25fbab699b6217718c25c4842249c54fa111d626
NasirUddin93 <nasir93cse@gmail.com>
2021-03-27 19:19:06 +0600

    Admin Making On Sale Timer Working

--- app/Http/Livewire/DetailsComponent.php
+++ app/Http/Livewire/DetailsComponent.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Product;
+use App\Models\Sale;
 use Cart;
 
 class DetailsComponent extends Component
@@ -22,7 +23,8 @@ class DetailsComponent extends Component
 		$product =Product::where('slug',$this->slug)->first();
 		$popular_product = Product::inRandomOrder()->limit(4)->get();
 		$related_product = Product::where('category_id',$product->category_id)->inRandomOrder()->limit(5)->get();
-        return view('livewire.details-component',['product'=>$product,'popular_product'=>$popular_product,'related_product'=>$related_product])->layout('layouts.base');
+        $sale = Sale::find(1);
+        return view('livewire.details-component',['product'=>$product,'popular_product'=>$popular_product,'related_product'=>$related_product,'sale'=>$sale])->layout('layouts.base');
     }
 }
 
